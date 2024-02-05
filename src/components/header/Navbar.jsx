7fbf8d156c1c31bb9ec57c4f2d415a90ce347175
adnanAhmad99 <0adnanahmad99@gmail.com>
2024-02-05 22:11:48 +0500

    fixed other issues

--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -1,12 +1,7 @@
-import { useState } from "react";
-
 const LINKSARRAY = ["About", "Skills", "Certificates", "Projects"];
 
 const Navbar = () => {
-  const [activeLink, setActiveLink] = useState("Home");
-
   const handleLink = (data) => {
-    setActiveLink(data);
     const idData =
       data === "Final note" ? "#final-note" : `#${data.toLowerCase()}`;
     const selectedId = document.querySelector(idData);
@@ -16,14 +11,7 @@ const Navbar = () => {
     <ul className="ul-navlinks">
       {LINKSARRAY.map((element, i) => (
         <li key={`${i}${element}`}>
-          <button
-            className={`links-button ${
-              activeLink === element ? "link-active" : ""
-            }`}
-            onClick={() => handleLink(element)}
-          >
-            {element}
-          </button>
+          <button onClick={() => handleLink(element)}>{element}</button>
         </li>
       ))}
     </ul>
